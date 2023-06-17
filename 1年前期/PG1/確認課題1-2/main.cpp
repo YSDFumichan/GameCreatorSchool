6ba098f398db6c6ff53f228deeef3bc752c28cc4
YoshidaFumichan <k020g1392@g.neec.ac.jp>
2023-06-17 21:11:59 +0900

    確認課題2-1 自己紹介

--- 1年前期/PG1/確認課題1-2/main.cpp
+++ 1年前期/PG1/確認課題1-2/main.cpp
@@ -1,9 +1,10 @@
-#include <stdio.h>
+#include <iostream>
 
-int main(void) {
-	const char* name = "吉田 文也";	// 名前
-	int age = 20;	// 年齢
-	float height = 166.7f;	// 身長
-	// %s = 文字列, %d = 整数(int), %f = 実数(float)
+int main() {
+	const char* name = "吉田文也";	// 名前
+	int age = 20;					// 年齢
+	float height = 166.7f;			// 身長
+	// %s = 文字列(const char*), %d = 整数(int), %f = 実数(float)%.1fは小数点1桁目まで
 	printf("%s（%d歳）身長（%.1fcm）\n", name, age, height);
+	return 0;
 }
