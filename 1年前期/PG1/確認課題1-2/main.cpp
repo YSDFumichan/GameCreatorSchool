fdd3b5a9adaa086b6705569f9f4e87eaa63ff3f8
YoshidaFumichan <k020g1392@g.neec.ac.jp>
2023-06-17 11:37:42 +0900

    修正

--- 1年前期/PG1/確認課題1-2/main.cpp
+++ 1年前期/PG1/確認課題1-2/main.cpp
@@ -4,6 +4,6 @@ int main(void) {
 	const char* name = "吉田 文也";	// 名前
 	int age = 20;	// 年齢
 	float height = 166.7f;	// 身長
-	//%s = 文字列, %d = 整数(int), %f = 実数(float)
+	// %s = 文字列, %d = 整数(int), %f = 実数(float)
 	printf("%s（%d歳）身長（%.1fcm）\n", name, age, height);
 }
